9d3ee7c0dc4fca3b319f2bd4d4ab64ed120c868e
Mehrdad Sabetzadeh <mehrdad.sabetzadeh@uni.lu>
2021-03-10 15:02:55 -0500

    no message

--- lecture15/05abc/Test.java
+++ lecture15/05abc/Test.java
@@ -1,27 +1,27 @@
-import java.io.*;
-
 public class Test {
     public static void c() {
         System.out.println( "c() :: about to throw exception" );
         throw new RuntimeException( "from c()" );
-        
+    }
+    public static void b() {
+        System.out.println( "b() :: pre-" );
+        c();
+        System.out.println( "b() :: post-" );
     }
     public static void a() {
         System.out.println( "a() :: pre-" );
         try {
-            c();
-        } catch ( NumberFormatException e ) {
+            b();
+        } catch ( RuntimeException e ) {
             System.out.println( "a() :: caught exception" );
-        } finally {
-            System.out.println( "finally" );
         }
         System.out.println( "a() :: calling b, no try block" );
-        c();
+        b();
         System.out.println( "a() :: post-" );
     }
     public static void main( String[] args ) {
-        System.out.println( "main( ... ) :: pre-" );
-        a();
-        System.out.println( "main( ... ) :: post-" );
-    }
+       System.out.println( "main( ... ) :: pre-"  );
+       a();
+       System.out.println( "main( ... ) :: post-" );
+    } 
 }
