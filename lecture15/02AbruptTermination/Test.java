3dc88150740db271a15c60b39911d76c1bfa6821
Mehrdad Sabetzadeh <mehrdad.sabetzadeh@uni.lu>
2021-03-14 21:01:09 -0400

    no message

--- lecture15/02AbruptTermination/Test.java
+++ lecture15/02AbruptTermination/Test.java
@@ -1,10 +1,19 @@
-class Test {
-    public static void main ( String[] args ) {
-        System.out.println( "-1-" );
+public class Test extends Object {
+    public static boolean error() {
         if ( true ) {
-            throw new RuntimeException( "Help!" );
-        }
+            throw new RuntimeException( "an Exception" );
+}
+        return true;
+    }
+    public static boolean display() {
         System.out.println( "-2-" );
+        return true;
+    }
+    public static void main( String[] args ) {
+        System.out.println( "-1-" );
+        if ( error() || display() ) {
+            System.out.println( "-3-" );
+        }
+        System.out.println( "-4-" );
     }
 }
-
